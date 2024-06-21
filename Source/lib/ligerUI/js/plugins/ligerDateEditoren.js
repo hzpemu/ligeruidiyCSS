9e2161d39100ed66e4934fe6a8476c909c56724e
hzpemu <hzpemu@gmail.com>
2024-06-21 09:53:25 +0800

    Update ligerDateEditoren.js

--- Source/lib/ligerUI/js/plugins/ligerDateEditoren.js
+++ Source/lib/ligerUI/js/plugins/ligerDateEditoren.js
@@ -73,11 +73,11 @@
             var dateeditorHTML = "";
             dateeditorHTML += "<div class='l-box-dateeditor' style='display:none'>";
             dateeditorHTML += "    <div class='l-box-dateeditor-header'>";
-            dateeditorHTML += "        <div class='l-box-dateeditor-header-btn l-box-dateeditor-header-prevyear'><span></span></div>";
-            dateeditorHTML += "        <div class='l-box-dateeditor-header-btn l-box-dateeditor-header-prevmonth'><span></span></div>";
+            dateeditorHTML += "        <div class='l-box-dateeditor-header-btn fa fa-step-backward'><span></span></div>";
+            dateeditorHTML += "        <div class='l-box-dateeditor-header-btn fa-arrow-circle-left'><span></span></div>";
             dateeditorHTML += "        <div class='l-box-dateeditor-header-text'><a class='l-box-dateeditor-header-month'></a> , <a  class='l-box-dateeditor-header-year'></a></div>";
-            dateeditorHTML += "        <div class='l-box-dateeditor-header-btn l-box-dateeditor-header-nextmonth'><span></span></div>";
-            dateeditorHTML += "        <div class='l-box-dateeditor-header-btn l-box-dateeditor-header-nextyear'><span></span></div>";
+            dateeditorHTML += "        <div class='l-box-dateeditor-header-btn fa-arrow-circle-right'><span></span></div>";
+            dateeditorHTML += "        <div class='l-box-dateeditor-header-btn fa fa-step-forward'><span></span></div>";
             dateeditorHTML += "    </div>";
             dateeditorHTML += "    <div class='l-box-dateeditor-body'>";
             dateeditorHTML += "        <table cellpadding='0' cellspacing='0' border='0' class='l-box-dateeditor-calendar'>";
